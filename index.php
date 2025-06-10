47dd3e706aeb9e3d136423e2af4ad1e2d72dcb3d
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2025-06-10 16:00:46 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['name'], $_POST['email'], $_POST['message'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+
+        // Koneksi ke database
+
+        $conn = new mysqli('localhost', 'root', '', 'form');
+        if ($conn->connect_error) {
+            die("Connection failed: {$conn->connect_error}");
+        } else {
+            $stmt = $conn->prepare("INSERT INTO re (name, email, message) VALUES (?, ?, ?)");
+            $stmt->bind_param("sss", $name, $email, $message);
+            $stmt->execute();
+            echo "<h1 class='success'>Data berhasil disimpan</h1>";
+            $stmt->close();
+            $conn->close();
+        }
+    }
+?>
+</body>
+</html>
